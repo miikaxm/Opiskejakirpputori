0698e12d4e4cd9a0938f9d47d77f9023cf2c09c6
Miika Valkonen <miika.valkonen08@gmail.com>
2025-12-17 11:02:37 +0200

    Viestitoiminto, sumennus formien avauksissa, formien tyylit tummaan teemaan sopivaksi ja koodin kommentointi sekä siistimistä

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const loginForm = document.getElementById("loginForm")
 const regForm = document.getElementById("registerForm")
 const createOfferForm = document.getElementById("createOffer")
+const sendMessage = document.getElementById("sendMessage")
 let loggedInUser = ""
 
 // Eventlistenerit kirjautumiselle ja rekisteröitymiselle
@@ -9,10 +10,14 @@ document.getElementById("registerForm").addEventListener("submit", register)
 document.getElementById("loginForm").addEventListener("submit", logIn)
 document.getElementById("createOffer").addEventListener("submit", createOffer)
 
+// Viestin lähetys
+document.getElementById("sendMessage").addEventListener("submit", sendNewMessage)
+
 // Formien avaukset
 document.getElementById("OpenLogIn").addEventListener("click", () => {
     if (loggedInUser === "") {
         loginForm.style.display = "block"
+        document.querySelector(".overlay").style.display = "block";
     } else {
         loggedInUser = ""
         updateState()
@@ -23,14 +28,41 @@ document.getElementById("OpenLogIn").addEventListener("click", () => {
 document.getElementById("openReg").addEventListener("click", () => {
     loginForm.style.display = "none"
     regForm.style.display = "block"
+    document.querySelector(".overlay").style.display = "block";
 })
 
-document.getElementById("createSellOffer").addEventListener("click", () => {createOfferForm.style.display = "block"})
+document.getElementById("openMessages").addEventListener("click", () => {
+    Messages.style.display = "block"
+    document.querySelector(".overlay").style.display = "block";
+    paivitaViestit()
+})
+
+document.getElementById("openSendMessage").addEventListener("click", () => {
+    sendMessage.style.display = "block"
+    Messages.style.display = "none"
+    document.querySelector(".overlay").style.display = "block";
+})
+
+document.getElementById("createSellOffer").addEventListener("click", () => {
+    createOfferForm.style.display = "block"
+    document.querySelector(".overlay").style.display = "block";
+})
 
 // Formien sulkemiset
-document.getElementById("closeLogIn").addEventListener("click", () => {loginForm.style.display = "none"})
-document.getElementById("closeReg").addEventListener("click", () => {regForm.style.display ="none"})
-document.getElementById("closeCreate").addEventListener("click", () => {createOfferForm.style.display = "none"})
+function closeForm(formId, overlaySelector = ".overlay") {
+    const form = document.getElementById(formId)
+    if (form) form.style.display = "none"
+    const overlay = document.querySelector(overlaySelector)
+    if (overlay) overlay.style.display = "none"
+}
+
+// Close form event listeners
+document.getElementById("closeLogIn").addEventListener("click", () => closeForm("loginForm"))
+document.getElementById("closeReg").addEventListener("click", () => closeForm("registerForm"))
+document.getElementById("closeCreate").addEventListener("click", () => closeForm("createOffer"))
+document.getElementById("closeMessages").addEventListener("click", () => closeForm("Messages"))
+document.getElementById("closeSendMessage").addEventListener("click", () => closeForm("sendMessage"))
+
 
 // Kirjautumis formin avaus rekisteröitymisestä
 document.getElementById("LoginToReg").addEventListener("click", () => {
@@ -38,22 +70,15 @@ document.getElementById("LoginToReg").addEventListener("click", () => {
     loginForm.style.display = "block"
 })
 
-function getUsers() {
-    return JSON.parse(localStorage.getItem("users") || "[]")
-}
+// Localstorage
+function getUsers() {return JSON.parse(localStorage.getItem("users") || "[]")}
+function saveUsers(users) {localStorage.setItem("users", JSON.stringify(users))}
+function getOffers() {return JSON.parse(localStorage.getItem("ilmoitukset")) || [];}
+function saveOffers(offers) {return localStorage.setItem("ilmoitukset", JSON.stringify(offers))}
+function getMessages() {return JSON.parse(localStorage.getItem("viestit")) || [];}
+function saveMessages(messages) {return localStorage.setItem("viestit", JSON.stringify(messages))}
 
-function saveUsers(users) {
-    localStorage.setItem("users", JSON.stringify(users))
-}
-
-function getOffers() {
-    return JSON.parse(localStorage.getItem("ilmoitukset")) || [];
-}
-
-function saveOffers (offers) {
-    return localStorage.setItem("ilmoitukset", JSON.stringify(offers))
-}
-
+// Funktio rekisteröitymiselle
 function register(event){
     event.preventDefault()
     const username = document.getElementById("regUsername").value.trim()
@@ -76,8 +101,10 @@ function register(event){
     regForm.style.display = "none"
     document.getElementById("regUsername").value = ""
     document.getElementById("regPassword").value = ""
+    document.querySelector(".overlay").style.display = "none";
 }
 
+// Funktio kirjautumiselle
 function logIn(event){
     event.preventDefault()
     const username = document.getElementById("logUsername").value.trim()
@@ -97,6 +124,7 @@ function logIn(event){
         loginForm.style.display = "none"
         document.getElementById("LogPassword").value = ""
         document.getElementById("logUsername").value = ""
+        document.querySelector(".overlay").style.display = "none";
         paivitaLista()
     } else {
         // Tähänkin parempi ilmoitus myöhemmin
@@ -105,17 +133,23 @@ function logIn(event){
     }
 }
 
+// Kirjautumisen tilan päivittäminen
 function updateState() {
     document.getElementById("loggedUsername").innerHTML = loggedInUser
     if (loggedInUser !== "") {
         document.getElementById("OpenLogIn").innerHTML = "Kirjaudu ulos"
         document.getElementById("createSellOffer").style.display = "block"
+        document.getElementById("openMessages").style.display = "block"
+        document.getElementById("openSendMessage").style.display = "block"
     } else {
         document.getElementById("OpenLogIn").innerHTML = "Kirjaudu sisään"
         document.getElementById("createSellOffer").style.display = "none"
+        document.getElementById("openMessages").style.display = "none"
+        document.getElementById("openSendMessage").style.display = "none"
     }
 }
 
+// Funktio myynti-ilmoituksen luonnille
 function createOffer(event) {
     event.preventDefault()
     const otsikko = document.getElementById("title").value
@@ -150,10 +184,42 @@ function createOffer(event) {
         document.getElementById("title").value = ""
         document.getElementById("price").value = ""
         photoInput.value = ""
+        document.querySelector(".overlay").style.display = "none";
         paivitaLista()
     }
 
     reader.readAsDataURL(file)
+}
+
+// Funktio viestin lähetykselle
+function sendNewMessage(event) {
+    event.preventDefault()
+    const vastaanottaja = document.getElementById("toUser").value.toLowerCase()
+    const lähettäjä = document.getElementById("loggedUsername").innerHTML.toLowerCase()
+    const viesti = document.getElementById("message").value
+    const users = getUsers()
+    const viestit = getMessages()
+
+    const user = users.find(u => u.username.toLowerCase() === vastaanottaja.toLowerCase())
+
+    if(!user) {
+        // Tähänkin parempi ilmoitus myöhemmin
+        alert("Vastaan ottajan käyttäjää ei löydy")
+        return
+    }
+
+    const NewMessage = {
+        vastaanottaja,
+        lähettäjä,
+        viesti
+    }
+
+    viestit.push(NewMessage)
+    saveMessages(viestit)
+    sendMessage.style.display = "none"
+    document.getElementById("toUser").value = ""
+    document.getElementById("message").value = ""
+    document.querySelector(".overlay").style.display = "none";
 }
 
 // Ilmoituslistan päivitys sivulle
@@ -175,7 +241,7 @@ function paivitaLista() {
             <div class="card">
                 <button class="removeBtn" onclick="poistaIlmoitus(${item.id})">×</button>
                 <img src="${item.kuva}" alt="${item.otsikko}">
-                <h2 class="card-title">${item.otsikko}</h2>
+                <h2 class="card-title">${item.otsikko} | Myyjä: ${item.creator}</h2>
                 <h3>Hinta: <strong>${item.hinta} €</strong></h3>
             </div>
         `;
@@ -183,7 +249,7 @@ function paivitaLista() {
             lista.innerHTML += `
             <div class="card">
                 <img src="${item.kuva}" alt="${item.otsikko}">
-                <h2 class="card-title">${item.otsikko}</h2>
+                <h2 class="card-title">${item.otsikko} | Myyjä: ${item.creator}</h2>
                 <h3>Hinta: <strong>${item.hinta} €</strong></h3>
             </div>
         `;
@@ -201,3 +267,25 @@ function poistaIlmoitus(id) {
 
 // Listan päivitys
 paivitaLista();
+
+// Viestien päivitys
+function paivitaViestit() {
+    const viestit = getMessages()
+    const viestiLista = document.getElementById("messageList")
+    viestiLista.innerHTML = ""
+
+    if (viestit.length === 0) {
+        viestiLista.innerHTML = `<p>Ei viestejä vielä.</p>`;
+        return;
+    }
+
+    viestit.forEach(viesti => {
+        if (viesti.vastaanottaja === loggedInUser.toLowerCase()) {
+            viestiLista.innerHTML += `
+            <div>
+                <p><strong>${viesti.lähettäjä}:</strong> ${viesti.viesti}</p>
+            </div>
+            `
+        }
+    })
+}
